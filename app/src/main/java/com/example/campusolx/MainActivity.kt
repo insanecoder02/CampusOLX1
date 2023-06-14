d0b2efc39302f23c346e0eb1fc5330e861846be0
SuhailAhmadKhan <suhailkhan568@gmail.com>
2023-06-14 07:57:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/campusolx/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.campusolx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.campusolx.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener{item->
+            when(item.itemId){
+                R.id.menu_home->{
+                    showHomeFragment()
+                    true
+                }
+                R.id.menu_account->{
+                    showAccountFragment()
+true
+                }
+
+                R.id.menu_my_ads->{
+                    showMyAdsFragment()
+true
+                }
+                R.id.menu_settings->{
+                    showSettingsFragment()
+true
+                }
+                else->{
+                    false
+                }
+
+            }
+
+
+        }
+
+    }
+    private fun showHomeFragment(){
+        binding.toolbarTitle.text="Home"
+        val fragment = HomeFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFl.id,fragment,"HomeFragment")
+        fragmentTransaction.commit()
+    }
+    private fun showAccountFragment(){
+        binding.toolbarTitle.text="Account"
+        val fragment = AccountFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFl.id,fragment,"AccountFragment")
+        fragmentTransaction.commit()
+    }
+    private fun showMyAdsFragment(){
+        binding.toolbarTitle.text="My Ads"
+        val fragment = MyAdsFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFl.id,fragment,"MyAdsFragment")
+        fragmentTransaction.commit()
+    }
+    private fun showSettingsFragment(){
+        binding.toolbarTitle.text="Settings"
+        val fragment = SettingsFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentsFl.id,fragment,"SettingsFragment")
+        fragmentTransaction.commit()
+    }
+}
---
+++ app/src/main/java/com/example/campusolx/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.campusolx
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        supportActionBar?.hide()
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val i = Intent(
+                this@SplashActivity,
+                LoginActivity::class.java
+            )
+            startActivity(i)
+            finish()
+        }, 3000)
+    }
+}
