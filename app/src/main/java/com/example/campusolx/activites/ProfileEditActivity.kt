5b75402f3cd6f873378af99b21c46fd4b3f664b0
SuhailAhmadKhan <suhailkhan568@gmail.com>
2023-07-16 16:11:54 +0530

    changes made

--- app/src/main/java/com/example/campusolx/activites/ProfileEditActivity.kt
+++ app/src/main/java/com/example/campusolx/activites/ProfileEditActivity.kt
@@ -1,8 +1,9 @@
-package com.example.campusolx.activites
+package com.example.campusolx.activities
 
 import android.app.Activity
 import android.content.ContentValues
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -18,11 +19,20 @@ import com.bumptech.glide.Glide
 import com.example.campusolx.R
 import com.example.campusolx.utils.Utils
 import com.example.campusolx.databinding.ActivityProfileEditBinding
-import java.lang.Exception
+import com.example.campusolx.dataclass.User
+import com.example.campusolx.dataclass.UserUpdateRequest
+import com.example.campusolx.interfaces.AuthApi
+import com.example.campusolx.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileEditActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityProfileEditBinding
+    private lateinit var binding: ActivityProfileEditBinding
     private var imageUri: Uri? = null
+    private lateinit var authApi: AuthApi
+    private lateinit var accessToken: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileEditBinding.inflate(layoutInflater)
@@ -35,134 +45,180 @@ class ProfileEditActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        binding.toolBarBackBtn.setOnClickListener{
+
+
+        val retrofit = RetrofitInstance.getRetrofitInstance()
+        authApi = retrofit.create(AuthApi::class.java)
+
+        binding.toolBarBackBtn.setOnClickListener {
             onBackPressed()
         }
-        binding.profileImagePickFab.setOnClickListener{
+        binding.profileImagePickFab.setOnClickListener {
             imagePickDialog()
         }
-        binding.updateBtn.setOnClickListener{
+        binding.updateBtn.setOnClickListener {
             validateData()
         }
-
+        val sharedPreference = getSharedPreferences("Account_Details", Context.MODE_PRIVATE)
+        accessToken = "Bearer " + sharedPreference.getString("accessToken", "") ?: ""
     }
+
     private var name = ""
-    private var rollno = ""
+    private var rollNo = ""
     private var email = ""
-    private var phoneCode = ""
-    private var phoneNumber = ""
+    private var semester = 0
+    private var branch = ""
+    private var contact = ""
+    private var upiId = ""
+    private var profilePicture = ""
 
-
-    private fun validateData(){
+    private fun validateData() {
         name = binding.nameEt.text.toString().trim()
-        rollno = binding.rollEt.text.toString().trim()
+        rollNo = binding.rollEt.text.toString().trim()
         email = binding.emailEt.text.toString().trim()
-        phoneCode = binding.countryCodePicker.selectedCountryCodeWithPlus
-        phoneNumber = binding.phoneNumberEt.text.toString().trim()
+        semester = 0 // Set the appropriate value for the semester
+        branch = "" // Set the appropriate value for the branch
+        contact = "" // Set the appropriate value for the contact
+        upiId = "" // Set the appropriate value for the UPI ID
+        profilePicture = "" // Set the appropriate value for the profile picture URL
 
-
+        updateUser()
     }
 
+    private fun updateUser() {
+        val userUpdateRequest = UserUpdateRequest(
+            name = name,
+            enrollmentNo = rollNo,
+            semester = semester,
+            upiId = upiId,
+            branch = branch,
+            contact = contact,
+            email = email,
+            profilePicture = profilePicture
+        )
 
-    private fun imagePickDialog(){
+
+
+        authApi.updateUser(accessToken, "user_id", userUpdateRequest)
+            .enqueue(object : Callback<User> {
+                override fun onResponse(call: Call<User>, response: Response<User>) {
+                    if (response.isSuccessful) {
+                        // User update successful, handle the response as needed
+                        val updatedUser = response.body()
+                        // Update the UI or perform any other operations with the updated user data
+                    } else {
+                        // User update failed, handle the error response
+                        val errorBody = response.errorBody()?.string()
+                        // Handle the error message appropriately
+                    }
+                }
+
+                override fun onFailure(call: Call<User>, t: Throwable) {
+                    // Handle the failure appropriately
+                }
+            })
+    }
+
+    private fun imagePickDialog() {
         val popupMenu = PopupMenu(this, binding.profileImagePickFab)
-        popupMenu.menu.add(Menu.NONE,1,1,"Camera")
-        popupMenu.menu.add(Menu.NONE,2,2,"Gallery")
+        popupMenu.menu.add(Menu.NONE, 1, 1, "Camera")
+        popupMenu.menu.add(Menu.NONE, 2, 2, "Gallery")
         popupMenu.show()
 
-        popupMenu.setOnMenuItemClickListener {item->
+        popupMenu.setOnMenuItemClickListener { item ->
             val itemId = item.itemId
-            if(itemId==1){
-                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+            if (itemId == 1) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                     requestCameraPermission.launch(arrayOf(android.Manifest.permission.CAMERA))
+                } else {
+                    requestCameraPermission.launch(
+                        arrayOf(
+                            android.Manifest.permission.CAMERA,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        )
+                    )
                 }
-                else{
-                    requestCameraPermission.launch(arrayOf(android.Manifest.permission.CAMERA,android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
-                }
-            }
-            else if(itemId==2){
-                if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+            } else if (itemId == 2) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                     pickImageGallery()
-                }
-                else{
+                } else {
                     requestStoragePermission.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
-
             }
 
             return@setOnMenuItemClickListener true
-
-        }
-
-    }
-    private val requestCameraPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){result->
-        var areAllGranted = true
-        for (isGranted in result.values){
-            areAllGranted = areAllGranted && isGranted
-        }
-        if(areAllGranted){
-            pickImageCamera()
-        } else{
-            Utils.toast(this, "Camera or Storage or both permissions denied")
-
-        }
-
-
-
-    }
-    private val requestStoragePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){isGranted ->
-        if(isGranted){
-            pickImageGallery()
-        }
-        else{
-            Utils.toast(this, "Storage Permission Denied")
-
         }
     }
-    private fun pickImageCamera(){
+
+    private val requestCameraPermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+            var areAllGranted = true
+            for (isGranted in result.values) {
+                areAllGranted = areAllGranted && isGranted
+            }
+            if (areAllGranted) {
+                pickImageCamera()
+            } else {
+                Utils.toast(this, "Camera or Storage or both permissions denied")
+            }
+        }
+
+    private val requestStoragePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                pickImageGallery()
+            } else {
+                Utils.toast(this, "Storage Permission Denied")
+            }
+        }
+
+    private fun pickImageCamera() {
         val contentValues = ContentValues()
-        contentValues.put(MediaStore.Images.Media.TITLE,"Temp_image_title")
-        contentValues.put(MediaStore.Images.Media.DESCRIPTION,"Temp_image_description")
+        contentValues.put(MediaStore.Images.Media.TITLE, "Temp_image_title")
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Temp_image_description")
 
-        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        imageUri =
+            contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         cameraActivityResultLauncher.launch(intent)
     }
-    private fun pickImageGallery(){
+
+    private fun pickImageGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         galleryActivityResultLauncher.launch(intent)
     }
-    private val cameraActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
-        if(result.resultCode == Activity.RESULT_OK){
-            try{
-                Glide.with(this)
-                    .load(imageUri)
-                    .placeholder(R.drawable.ic_person)
-                    .into(binding.profileTv)
-            }
-            catch (e: Exception){
-                Log.e(TAG,"cameraActivityResultLauncher",e)
-            }
-        }
-        else{
-            Utils.toast(this, "Cancelled")
-        }
-    }
-    private val galleryActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
-        if(result.resultCode == Activity.RESULT_OK){
-            val data = result.data
-            imageUri = data!!.data
-            try{
-                Glide.with(this)
-                    .load(imageUri)
-                    .placeholder(R.drawable.ic_person)
-                    .into(binding.profileTv)
-            }
-            catch (e: java.lang.Exception){
-                Log.e(TAG, "galleryActivityResultLauncher",e)
+
+    private val cameraActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                try {
+                    Glide.with(this)
+                        .load(imageUri)
+                        .placeholder(R.drawable.ic_person)
+                        .into(binding.profileTv)
+                } catch (e: Exception) {
+                    Log.e(TAG, "cameraActivityResultLauncher", e)
+                }
+            } else {
+                Utils.toast(this, "Cancelled")
             }
         }
-    }
+
+    private val galleryActivityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                imageUri = data!!.data
+                try {
+                    Glide.with(this)
+                        .load(imageUri)
+                        .placeholder(R.drawable.ic_person)
+                        .into(binding.profileTv)
+                } catch (e: java.lang.Exception) {
+                    Log.e(TAG, "galleryActivityResultLauncher", e)
+                }
+            }
+        }
 }
--- app/src/main/java/com/example/campusolx/fragments/MyAdsFragment.kt
+++ app/src/main/java/com/example/campusolx/fragments/MyAdsFragment.kt
@@ -1,23 +1,119 @@
 package com.example.campusolx.fragments
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.campusolx.R
-
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.campusolx.RetrofitInstance
+import com.example.campusolx.adapters.AdapterAd
+import com.example.campusolx.dataclass.Product
+import com.example.campusolx.databinding.FragmentMyAdsBinding
+import com.example.campusolx.interfaces.ProductApi
+import com.example.campusolx.models.ModelAd
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.text.SimpleDateFormat
 
 class MyAdsFragment : Fragment() {
+    private lateinit var binding: FragmentMyAdsBinding
+    private lateinit var mContext: Context
+    private lateinit var productApi: ProductApi
+    private lateinit var adapterAd: AdapterAd
+    private var adArrayList: ArrayList<ModelAd> = ArrayList()
+    private lateinit var accessToken: String
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_ads, container, false)
+    ): View {
+        binding = FragmentMyAdsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Initialize the Retrofit ProductApi
+        val retrofit = RetrofitInstance.getRetrofitInstance()
+        productApi = retrofit.create(ProductApi::class.java)
 
+        // Get the access token from shared preferences
+        val sharedPreference =
+            mContext.getSharedPreferences("Account_Details", Context.MODE_PRIVATE)
+        accessToken = "Bearer " + sharedPreference.getString("accessToken", "") ?: ""
+
+        // Setup RecyclerView and Adapter
+        adapterAd = AdapterAd(mContext, adArrayList)
+        binding.adsRv.layoutManager = LinearLayoutManager(mContext)
+        binding.adsRv.adapter = adapterAd
+
+        // Fetch and display the products of the current user
+       // fetchMyAds()
+    }
+
+//    private fun fetchMyAds() {
+//        val call = productApi.getAllProductsOfUser(accessToken)
+//        call.enqueue(object : Callback<List<Product>> {
+//            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+//                if (response.isSuccessful) {
+//                    val productList = response.body()
+//                    productList?.let { products ->
+//                        adArrayList.clear()
+//                        for (product in products) {
+//                            val ad = ModelAd().apply {
+//                                id = product._id
+//                                uid = product.createdBy
+//                                brand = product.name
+//                                category = product.category
+//                                price = product.price.toString()
+//                                title = product.name
+//                                description = product.description
+//                                status = if (product.isSold) "Sold" else "Available"
+//                                timestamp = product.createdAt?.let {
+//                                    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX")
+//                                    dateFormat.parse(it)?.time ?: 0L
+//                                } ?: 0L
+//                                latitude = 0.0
+//                                longitude = 0.0
+//                                imageList = ArrayList(product.images)
+//                            }
+//                            adArrayList.add(ad)
+//                        }
+//                        adapterAd.notifyDataSetChanged()
+//                    }
+//                } else {
+//                    // Handle error case
+//                    val errorMessage = response.message()
+//                    // Show an error message or handle the error response
+//                    // For example, you can display a toast message with the error
+//                    Log.e("Fetch", "Failed to fetch products: $errorMessage")
+//                    Toast.makeText(context, "Error: $errorMessage", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+//                // Handle failure case
+//                // Show an error message or handle the failure
+//                // For example, you can display a toast message with the failure
+//                Log.e("Fetch", "Failed to fetch products: ${t.message}")
+//                Toast.makeText(
+//                    context,
+//                    "Failed to fetch products: ${t.message}",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//        })
+//    }
+}
